84afb3163441a542780d937adfc15aec9fce1824
Hana <momenthana@kakao.com>
2021-12-06 14:13:53 +0000

    init

---
+++ precache-manifest.c26e4319d0313732159028b2c6875282.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0736dca9462cd13f6f43",
+    "url": "/css/chunk-26ecc12f.f4f85dcc.css"
+  },
+  {
+    "revision": "0409410d51e1225819d6",
+    "url": "/css/chunk-vendors.c439af37.css"
+  },
+  {
+    "revision": "4aad50fbb6dea438e8d87816307cc730",
+    "url": "/img/1.4aad50fb.png"
+  },
+  {
+    "revision": "a3498c7f7863928819fa183ba9bbae57",
+    "url": "/img/2.a3498c7f.png"
+  },
+  {
+    "revision": "cdaf2263a7c888ef504692866da8ce0b",
+    "url": "/img/3.cdaf2263.png"
+  },
+  {
+    "revision": "4051fa94dbe7b93be8189751f2b9d18f",
+    "url": "/img/4.4051fa94.png"
+  },
+  {
+    "revision": "ff55519a111dc9363f1add7c5669983d",
+    "url": "/img/5.ff55519a.png"
+  },
+  {
+    "revision": "ed87398a6f4bc9f261bbeb56a11681e5",
+    "url": "/img/6.ed87398a.png"
+  },
+  {
+    "revision": "4d8c32bc117fffb1e0a378c2a14e20b4",
+    "url": "/img/7.4d8c32bc.png"
+  },
+  {
+    "revision": "723eb6919ad1eb58e96262ce587cc29b",
+    "url": "/img/8.723eb691.png"
+  },
+  {
+    "revision": "4fab189d881613d3bb5055f2762a50d8",
+    "url": "/img/d.4fab189d.png"
+  },
+  {
+    "revision": "3b8a17d58f58fcc18aaf6ccfd4ee58ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eecc9d798a13797fd333",
+    "url": "/js/app.271c60d3.js"
+  },
+  {
+    "revision": "0736dca9462cd13f6f43",
+    "url": "/js/chunk-26ecc12f.9f9bea13.js"
+  },
+  {
+    "revision": "0409410d51e1225819d6",
+    "url": "/js/chunk-vendors.eae437ee.js"
+  },
+  {
+    "revision": "a523a71403a5e4440378783edcbf25f7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
